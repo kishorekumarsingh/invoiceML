6d5038b79af86f482ed2772537af5c321fd244e2
Kishore Kumar Singh <kishore.kumar@getmega.com>
2021-06-26 14:57:38 +0530

    added scatter plot for amount VS days past due date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Following this medium article for EDA
 # https://towardsdatascience.com/exploratory-data-analysis-in-python-c9a77dfa39ce
-
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -58,7 +58,6 @@ unique_df = convertToDate(unique_df, {
     }
 )
 unique_df = unique_df[unique_df["document_type"].str.contains("RV")].drop(["document_type"], axis=1)
-
 unique_df.to_csv("final_dataset.csv")
 
 # Add a column for difference in due_date and clear_date
@@ -70,48 +69,50 @@ Q3 = unique_df["diff"].quantile(0.99)
 unique_df = unique_df[(unique_df["diff"] > Q1) & (unique_df["diff"] < Q3)]
 
 
-# fig, axes = plt.subplots(1, 2, figsize=(18, 6))
-#
-# ax = axes[0]
-# df = unique_df["currency"].value_counts()
-# ax.pie(df, autopct='%1.1f%%', labels=df.index, startangle=30)
-# ax.legend()
-#
-# ax = axes[1]
-# df = unique_df["business_code"].value_counts(normalize=True)
-# ax.pie(df, autopct='%1.1f%%', labels=df.index, startangle=30)
-# ax.legend()
-#
-# plt.show()
-#
-# df = unique_df[{"currency", "diff"}]
-# df = df.groupby(['currency'])['diff'].mean()
-# ax = df.plot(kind='bar', figsize=(20, 10), color="indigo", fontsize=13)
-# ax.set_alpha(0.8)
-# ax.set_title("Days-past-due VS currency", fontsize=22)
-# ax.set_xlabel("currency", fontsize=15)
-# ax.set_ylabel("Number of days past due date", fontsize=15)
-# plt.show()
-#
-# df = unique_df[{"business_code", "diff"}]
-# df = df.groupby(['business_code'])['diff'].mean()
-# ax = df.plot(kind='bar', figsize=(20, 10), color="indigo", fontsize=13)
-# ax.set_alpha(0.8)
-# ax.set_title("Days-past-due VS business_code", fontsize=22)
-# ax.set_xlabel("business_code", fontsize=15)
-# ax.set_ylabel("Number of days past due date", fontsize=15)
-# plt.show()
-#
-#
-# df = unique_df[{"cust_payment_terms", "diff"}]
-# df = df.groupby(['cust_payment_terms'])['diff'].mean()
-# ax = df.plot(kind='bar', figsize=(20, 10), color="indigo", fontsize=13)
-# ax.set_alpha(0.8)
-# ax.set_title("Days-past-due VS cust_payment_terms", fontsize=22)
-# ax.set_xlabel("cust_payment_terms", fontsize=15)
-# ax.set_ylabel("Number of days past due date", fontsize=15)
-# plt.show()
-#
-#
+fig, axes = plt.subplots(1, 2, figsize=(18, 6))
+
+ax = axes[0]
+df = unique_df["currency"].value_counts()
+ax.pie(df, autopct='%1.1f%%', labels=df.index, startangle=30)
+ax.legend()
+
+ax = axes[1]
+df = unique_df["business_code"].value_counts(normalize=True)
+ax.pie(df, autopct='%1.1f%%', labels=df.index, startangle=30)
+ax.legend()
+
+plt.show()
+
+df = unique_df[{"currency", "diff"}]
+df = df.groupby(['currency'])['diff'].mean()
+ax = df.plot(kind='bar', figsize=(20, 10), color="indigo", fontsize=13)
+ax.set_alpha(0.8)
+ax.set_title("Days-past-due VS currency", fontsize=22)
+ax.set_xlabel("currency", fontsize=15)
+ax.set_ylabel("Number of days past due date", fontsize=15)
+plt.show()
+
+df = unique_df[{"business_code", "diff"}]
+df = df.groupby(['business_code'])['diff'].mean()
+ax = df.plot(kind='bar', figsize=(20, 10), color="indigo", fontsize=13)
+ax.set_alpha(0.8)
+ax.set_title("Days-past-due VS business_code", fontsize=22)
+ax.set_xlabel("business_code", fontsize=15)
+ax.set_ylabel("Number of days past due date", fontsize=15)
+plt.show()
 
 
+df = unique_df[{"cust_payment_terms", "diff"}]
+df = df.groupby(['cust_payment_terms'])['diff'].mean()
+ax = df.plot(kind='bar', figsize=(20, 10), color="indigo", fontsize=13)
+ax.set_alpha(0.8)
+ax.set_title("Days-past-due VS cust_payment_terms", fontsize=22)
+ax.set_xlabel("cust_payment_terms", fontsize=15)
+ax.set_ylabel("Number of days past due date", fontsize=15)
+plt.show()
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.scatter(unique_df["amount"], unique_df["diff"], c=unique_df["diff"])
+ax.set_xlabel("amount")
+ax.set_ylabel("days past due")
+plt.show()
